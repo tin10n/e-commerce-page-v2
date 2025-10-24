a8b70436254263c81ea99c134887241fab6c1e7d
Thanh Nay <218096274+tin10n@users.noreply.github.com>
2025-10-24 12:47:49 -0400

    added route for slider

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
+// Products 
 router.get('/', async (req, res) => {
   try {
     const client = await db.connect();    
@@ -11,6 +12,26 @@ router.get('/', async (req, res) => {
     res.json(result.rows);
   } catch (err) {
     console.error(" Query error:", err.message);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Slider
+  router.get('/new', async (req, res) => {
+  const limit = parseInt(req.query.limit) || 7;
+    try {
+    const client = await db.connect();
+    // Grabbing 7 products
+    const query = `
+      SELECT id, name, type, description, price, image
+      FROM products
+      LIMIT $1
+    `;
+    const result = await client.query(query, [limit]);
+    client.release();
+    res.json(result.rows);
+  } catch (err) {
+    console.error("New arrivals query error:", err.message);
     res.status(500).json({ error: err.message });
   }
 });
