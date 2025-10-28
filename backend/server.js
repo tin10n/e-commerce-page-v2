845226e20d21927d46bbc1f23752d1c2615aaeec
Thanh Nay <218096274+tin10n@users.noreply.github.com>
2025-10-28 00:02:35 -0400

    updated path for render

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
+app.use(cors({
+  origin: 'https://e-commerce-page-v2-frontend.onrender.com',
+  methods: ['GET', 'POST']
+}));
 
 // Use the route
 app.use('/api/products', productRoutes);
